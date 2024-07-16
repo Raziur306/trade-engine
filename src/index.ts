181a64ae83060b47fea75aae1f0586d4c7237962
Raziur306 <raziur.rahaman.ronju@gmail.com>
2024-07-16 21:05:06 +0600

    revert back

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,15 @@
-import { adminBot, userBot } from "./api/telegraf";
-import env from "./config/env";
-import app from "./s-config";
+import express from "express";
 
-userBot
-  .launch()
-  .then(() => {
-    console.log(`User bot is running on port ${env.PORT}`);
-  })
-  .catch((error) => {
-    console.error("Error launching the user bot:", error);
-  });
+const app = express();
 
-adminBot
-  .launch()
-  .then(() => {
-    console.log("Admin bot is running");
-  })
-  .catch((error) => {
-    console.error("Error launching the admin bot:", error);
-  });
-
-app.listen(env.PORT || 8080, () => {
-  console.log(`Server is running on port ${env.PORT}🔥🔥`);
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
 
-app.use("*", (req, res) => {
-  res.status(200).send({ message: "I'm your server" });
+const router = express.Router();
+
+router.get("*", (req, res) => {
+  res.send("Hello World!");
 });
+
+app.use(router);
